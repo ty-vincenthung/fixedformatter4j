a83503002664dce9cbe7a01c1945e103f40393e1
Vincent Hung <vincent.hung@iaspec.com>
2021-02-17 11:52:38 +0800

    add format method in GenericListFormatter

--- fixedformatter4j/src/main/java/com/github/vincenthung/fixedformatter4j/formatter/impl/GenericListFormatter.java
+++ fixedformatter4j/src/main/java/com/github/vincenthung/fixedformatter4j/formatter/impl/GenericListFormatter.java
@@ -3,6 +3,9 @@ package com.github.vincenthung.fixedformatter4j.formatter.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+
+import com.ancientprogramming.fixedformat4j.annotation.Align;
 import com.ancientprogramming.fixedformat4j.exception.FixedFormatException;
 import com.ancientprogramming.fixedformat4j.format.FixedFormatter;
 import com.ancientprogramming.fixedformat4j.format.FormatContext;
@@ -21,7 +24,6 @@ public class GenericListFormatter<T> implements FixedFormatter<List<T>> {
 	@SuppressWarnings("unchecked")
 	public List<T> parse(String value, FormatInstructions instructions) throws FixedFormatException {
 
-		System.out.println(instructions);
 		if (!(instructions instanceof FixedFormatterInstruction))
 			throw new FixedFormatException("Please use ExtendedFixedFormatManagerImpl instead of FixedFormatManagerImpl for List parsing");
 
@@ -39,24 +41,58 @@ public class GenericListFormatter<T> implements FixedFormatter<List<T>> {
 		List<T> result = new ArrayList<T>();
 		FixedFormatter<?> formatter = getElementFormatter(new FormatContext<T>(0, (Class<T>) formatList.elementClass(), formatList.elementFormatter()));
 		for (int offset = 0; offset < instrn.getLength(); offset += formatList.eachLength()) {
-			result.add((T) formatter.parse(value.substring(offset, offset + formatList.eachLength()),
-					new FormatInstructions(0, formatList.align(), formatList.paddingChar(),
+			Object parsedData = formatter.parse(value.substring(offset, offset + formatList.eachLength()),
+					new FormatInstructions(formatList.eachLength(), formatList.align(), formatList.paddingChar(),
 							instrn.getFixedFormatPatternData(), instrn.getFixedFormatBooleanData(),
-							instrn.getFixedFormatNumberData(), instrn.getFixedFormatDecimalData())));
-
+							instrn.getFixedFormatNumberData(), instrn.getFixedFormatDecimalData()));
+			if (parsedData != null) result.add((T) parsedData);
 		}
-
 		return result;
 	}
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public String format(List<T> value, FormatInstructions instructions) throws FixedFormatException {
 
 		if (!(instructions instanceof FixedFormatterInstruction)) {
 			throw new FixedFormatException("Please use ExtendedFixedFormatManagerImpl instead of FixedFormatManagerImpl for List formatting");
 		}
-		FixedFormatterInstruction instrn = (FixedFormatterInstruction) instructions;
 
-		return null;
+		FixedFormatterInstruction instrn = (FixedFormatterInstruction) instructions;
+		FixedFormatList formatList = instrn.getFixedFormatList();
+		if (formatList == null)
+			throw new FixedFormatException(String.format("%s must be annotated to the getter Generic List formatting", FixedFormatList.class.getName()));
+
+		if (formatList.eachLength() <= 0)
+			throw new FixedFormatException("The eachLength must be greater than 0 for list formatting");
+
+		if (instrn.getLength() % formatList.eachLength() != 0)
+			throw new FixedFormatException("The length is not a multiple of eachLength for list formatting");
+
+		FixedFormatter formatter = getElementFormatter(new FormatContext<T>(0, (Class<T>) formatList.elementClass(), formatList.elementFormatter()));
+
+		StringBuilder sb = new StringBuilder();
+		for (T element : value) {
+			String formattedStr = formatter.format(element,
+					new FormatInstructions(formatList.eachLength(), formatList.align(), formatList.paddingChar(),
+							instrn.getFixedFormatPatternData(), instrn.getFixedFormatBooleanData(),
+							instrn.getFixedFormatNumberData(), instrn.getFixedFormatDecimalData()));
+			if (formattedStr != null) {
+				if (formattedStr.length() < formatList.eachLength())
+					sb.append(formatList.align() == Align.LEFT ? StringUtils.rightPad(formattedStr, formatList.eachLength(), formatList.paddingChar())
+							: StringUtils.leftPad(formattedStr, formatList.eachLength(), formatList.paddingChar()));
+				else
+					sb.append(formattedStr);
+			}
+		}
+
+		String result = sb.toString();
+		if (result.length() < instrn.getLength())
+			result = StringUtils.leftPad(result, instrn.getLength(), instrn.getPaddingChar());
+
+		if (result.length() > instrn.getLength())
+			throw new FixedFormatException(String.format("List size %s with eachLength=%s exceed the total length=%s",
+					value.size(), formatList.eachLength(), instrn.getLength()));
+		return result;
 	}
 
 	protected FixedFormatter<?> getElementFormatter(FormatContext<T> context) {
